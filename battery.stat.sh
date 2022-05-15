550ad650731514f8e912c9aadc58c00d0c10abad
Michael Hancock <onykage@gmail.com>
2022-05-14 19:56:22 -0500

    Fixed no battery issue.

--- battery.stat.sh
+++ battery.stat.sh
@@ -4,34 +4,56 @@
 #
 # source = basicallytech.com/blog/archive/110/Colour-coded-battery-charge-level-and-status-in-your-bash-prompt
 
-#BATTERY=/proc/acpi/battery/BAT0
-#BATTERY=/sys/class/power_supply/BAT0
-#BATTERY=`acpi -a -b | grep Battery`
+## old way
+##BATTERY=/proc/acpi/battery/BAT0
 
-BATTERY=`acpi -b | cut -d',' -f 2 | egrep -o '[0-9]{2}'`
-CHARGE=${BATTERY//[[:blank:]]/}
-STATE=`acpi -b | cut -d',' -f 1 | cut -d':' -f 2`
-BATSTATE=${STATE//[[:blank:]]/}
 
 #REM_CAP=`grep "^remaining capacity" $BATTERY/state | awk '{ print $3 }'`
 #FULL_CAP=`grep "^last full capacity" $BATTERY/info | awk '{ print $4 }'`
 #BATSTATE=`grep "^charging state" $BATTERY/state | awk '{ print $3 }'`
 
+
 #CHARGE=`echo $(( $REM_CAP * 100 / $FULL_CAP ))`
+
+##new way
+
+BATTERY=/sys/class/power_supply/BAT1
+if [ -f "$BATTERY" ]; then
+
+	BATSTATE=`cat $BATTERY/status`
+
+	CHARGE=`cat $BATTERY/capacity`
+else
+
+	BATSTATE="full"
+	CHARGE="-1"
+fi
 
 NON='\033[00m'
 BLD='\033[01m'
 RED='\033[01;31m'
 GRN='\033[01;32m'
-YEL='\033[01;33m\a'
+YEL='\033[01;33m'
 
 COLOUR="$RED"
+
+#case "${BATSTATE}" in
+#	'charged')
+#	BATSTT="$BLD=$NON"
+#	;;
+#	'charging')
+#	BATSTT="$BLD+$NON"
+#	;;
+#	'discharging')
+#	BATSTT="$BLD-$NON"
+#	;;
+#esac
 
 case "${BATSTATE}" in
 	'Full')
 	BATSTT="$BLD=$NON"
 	;;
-	'Charging')
+	'Charging')	
 	BATSTT="$BLD+$NON"
 	;;
 	'Discharging')
@@ -41,7 +63,7 @@ esac
 
 #prevent a charge of more then 100% displaying
 
-if [ "$CHARGE" == "00" ]
+if [ "$CHARGE" -gt "99" ]
 then
 	CHARGE=100
 fi
@@ -55,8 +77,16 @@ if [ "$CHARGE" -gt "30" ]
 then
 	COLOUR="$GRN"
 fi
+if [ "$CHARGE" -eq "-1" ]
+then
+	COLOUR="$NON"
+	CHARGE="NOBATT"
+	echo -e "${COLOUR}${CHARGE}${NON}${BATSTT}"
+else
+	echo -e "${COLOUR}${CHARGE}%${NON} ${BATSTT}"
+fi
 
 
-echo -e "${COLOUR}${CHARGE}%${NON} ${BATSTT}\a"
+
 
 #end of file
